236db460e2a9674f9e2008f030252ab3bc2b8e75
RapidSpell <142364086+RapidSpell@users.noreply.github.com>
2026-03-12 09:39:54 +1300

    Add files via upload

--- 01_Colour_Quest_V1.py
+++ 01_Colour_Quest_V1.py
@@ -1,6 +1,5 @@
 from tkinter import *
-
-from click import command
+from functools import partial # To prevent unwanted windows
 
 
 class Converter:
@@ -17,8 +16,8 @@ class Converter:
         self.start_frame.grid()
 
         # strings for labels
-        self.intro_string = ("In each round you will be invited to chose a"
-                       "colour. Your goal is to beat the target score"
+        self.intro_string = ("In each round you will be invited to chose a "
+                       "colour. Your goal is to beat the target score "
                        "and win the round (and keep your points)")
 
         # chose_string = "opps - please chose a whole number more than zero."
@@ -49,22 +48,25 @@ class Converter:
         self.num_rounds = Spinbox(self.entry_area_frame, from_=1, to=100)
         self.num_rounds.grid(row=0, column=0)
 
-        self.num_rounds_button = Button(self.entry_area_frame, text="Play",
+        self.play_button = Button(self.entry_area_frame, text="Play",
                                         font=("Arial", 12, "bold"),
                                         fg="#ffffff", bg="#0057D8", width=10,
                                         command=self.to_play)
-        self.num_rounds_button.grid(row=0, column=1)
+        self.play_button.grid(row=0, column=1)
 
 
     def to_play(self):
         """Retrieves the value from the associated Tkinter variable."""
         # Access the value directly from the Tkinter variable
-        rounds = self.num_rounds.get()
         try:
-            rounds = int(rounds)
+            rounds = int(self.num_rounds.get())
 
             if 0 < rounds < 100:
-                PlayGame(self, self.num_rounds)
+                # send user to PlayGame class to start the game
+                PlayGame(rounds)
+
+                # remove box for number of games select
+                root.withdraw()
 
             else:
 
@@ -79,9 +81,35 @@ class PlayGame:
     """
     runs game
     """
-    def __init__(self, partner, num_games):
-        print(num_games)
 
+    def __init__(self, num_games):
+        """
+        create the game GUI the game
+        """
+
+        # setup dialogue box
+        self.game_box = Toplevel()
+
+        self.play_frame = Frame(padx=10, pady=10)
+        self.play_frame.grid()
+
+        self.temp_num_games = num_games
+
+        while self.temp_num_games > 0:
+
+            # create games played label
+            self.games_label = (f"Round {self.temp_num_games} of {num_games}\n"
+                                f"You Have {num_games - self.temp_num_games} "
+                                f"rounds left")
+
+            # Create label show what game number they are on
+            self.play_label = Label(self.play_frame, text=self.games_label, fg="#000000", font="Arial 14")
+            self.play_label.grid(row=0, column=0)
+
+            # change games left counter
+            self.temp_num_games -= 1
+
+        root.mainloop()
 
 
 # main routine
