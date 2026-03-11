092d54d809d6520d6d93db1d4b5233b4fc1c3ad2
RapidSpell <142364086+RapidSpell@users.noreply.github.com>
2026-03-12 10:36:46 +1300

    Add files via upload

--- 01_Colour_Quest_V1.py
+++ 01_Colour_Quest_V1.py
@@ -45,13 +45,13 @@ class Converter:
         self.entry_area_frame = Frame(self.start_frame)
         self.entry_area_frame.grid(row=3)
 
-        self.num_rounds = Spinbox(self.entry_area_frame, from_=1, to=100)
+        self.num_rounds = Entry(self.entry_area_frame, font="Arial 19")
         self.num_rounds.grid(row=0, column=0)
 
         self.play_button = Button(self.entry_area_frame, text="Play",
-                                        font=("Arial", 12, "bold"),
-                                        fg="#ffffff", bg="#0057D8", width=10,
-                                        command=self.to_play)
+                                  font=("Arial", 12, "bold"), fg="#ffffff",
+                                  bg="#0057D8", width=10,
+                                  command=self.to_play)
         self.play_button.grid(row=0, column=1)
 
 
@@ -77,37 +77,51 @@ class Converter:
             self.label_ref_list[2].config(text="Please enter a Valid integer", fg="#880808",
                                           font="Arial 14 bold")
 
+
 class PlayGame:
     """
     runs game
     """
 
-    def __init__(self, num_games):
+    def __init__(self, num_games, temp_games=0):
         """
         create the game GUI the game
         """
 
-        # setup dialogue box
-        self.game_box = Toplevel()
+        # create variable to track how many rounds left to play
+        temp_games = num_games
 
-        self.play_frame = Frame(self.game_box, padx=10, pady=10)
-        self.play_frame.grid()
+        # while loop to continue to play rounds until no more rounds left
+        while temp_games > 0:
 
-        self.temp_num_games = num_games
+            # setup dialogue box
+            self.play_box = Toplevel()
 
-        while self.temp_num_games > 0:
+            self.play_frame = Frame(self.play_box, padx=10, pady=10)
+            self.play_frame.grid()
+
+            # change games left counter
+            temp_games -= 1
 
             # create games played label
-            self.games_label = (f"Round {self.temp_num_games} of {num_games}\n"
-                                f"You Have {num_games - self.temp_num_games} "
-                                f"rounds left")
+            self.games_label = (f"Round {num_games - temp_games} of {num_games}\n"
+                                f"You Have {temp_games} rounds left")
 
             # Create label show what game number they are on
             self.play_label = Label(self.play_frame, text=self.games_label, fg="#000000", font="Arial 14")
             self.play_label.grid(row=0, column=0)
 
-            # change games left counter
-            self.temp_num_games -= 1
+            self.end_game_button = Button(self.play_frame, text="End",fg="#000000",
+                                          bg="#ff0000", width=5, height=1, font=("Arial", 16, "bold"),
+                                          command=self.close_play)
+            self.end_game_button.grid(row=1)
+
+    def close_play():
+        # send user back to round select page
+        Converter()
+
+        # remove box for number of games select
+        root.withdraw()
 
 
 # main routine
